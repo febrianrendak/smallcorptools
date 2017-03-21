72088ec91e37b692c623acd1c780d4299de1ec7d
Mohammad Riftadi <riftadi@gmail.com>
2017-03-21 11:22:24 +0700

    del ssl

--- views.py
+++ views.py
@@ -15,9 +15,6 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.message import MIMEMessage
 
-from OpenSSL import SSL
-context = ('/home/centos/smallcorptools/ssl_cert/cert.pem', '/home/centos/smallcorptools/ssl_cert/cert.pem')
-
 client = MongoClient('mongodb://localhost:27017/')
 dbh = client.jawdat_internal
 
@@ -771,4 +768,4 @@ def rpext(reset_url):
     else:
         abort(401)
 
-app.run(debug=True, host="0.0.0.0", port=5000, threaded=True, ssl_context=context)
+app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
